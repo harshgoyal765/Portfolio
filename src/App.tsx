c4c84280c7113321328ec10ad5f63501fa04c74a
harsh goyal <18harshgoyal@gmail.com>
2026-01-14 14:19:11 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index.js";
-import NotFound from "./pages/NotFound.js";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
 
 const queryClient = new QueryClient();
 
